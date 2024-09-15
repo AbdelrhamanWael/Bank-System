5e8b21c951dba5f00911d283befa00301f19f870
youssefahmed10-top <yousssefahmed9@gmail.com>
2024-09-15 18:56:07 +0300

    Phase _1

--- Bank-System/Route-Project-main/Bank System.h
+++ Bank-System/Route-Project-main/Bank System.h
@@ -1,251 +1,347 @@
-
 #pragma once
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 #include <iostream>
-using namespace std;
 #include <string>
 #include <cctype>
-
+using namespace std;
 
 class Person
 {
 protected:
-	//attribute:
-	string name;
-	string password;
-	int id;
+    // attributes:
+    string name;
+    string password;
+    int id;
 
 public:
-	Person() {
-		this->id = 0;
-	};
-	Person(string name, string password, int id) {
-		this->name = name;
-		this->password = password;
-		this->id = id;
-	};
+    Person()
+    {
+        this->id = 0;
+    }
+    Person(string name, string password, int id)
+    {
+        this->name = name;
+        this->password = password;
+        this->id = id;
+    }
 };
-
-
 class Client : public Person
 {
-protected:
-	//attribute:
-	double balance;
+private:
+    // attribute:
+    double balance;
 
 public:
-	//constractor:
-	Client() {
-		this->balance = 0;
-	};
-	Client(string name, string password, int id, double balance)
-		:Person(name, password, id) {
+    // constractor:
+    Client()
+    {
+        this->balance = 0;
+    };
+    Client(string name, string password, int id, double balance)
+        : Person(name, password, id)
+    {
 
-		if (balance < 1500) {
-			cout << "the minimum of balance is 1500 !";
-			return;
-		}
-		else {
-			this->balance = balance;
-		}
-	}
+        if (balance < 1500)
+        {
+            cout << "the minimum of balance is 1500 !";
+            return;
+        }
+        else
+        {
+            this->balance = balance;
+        }
+    }
 
-	//setter:
-	void setName(string name) {
+    // setter:
+    void setName(string name)
+    {
 
-		int count = 0;
+        int count = 0;
 
-		if (name.length() < 5 || name.length() > 20) {
-			cout << "the name must be between 5 to 20 latter !" << endl;
-			return;
-		}
+        if (name.length() < 5 || name.length() > 20)
+        {
+            cout << "the name must be between 5 to 20 latter !" << endl;
+            return;
+        }
 
-		else {
-			for (int i = 0; i < name.size(); i++) {
-				if (isalpha(name[i])) {
-					count++;
-				}
-			}
-			if (count != name.length()) {
-				cout << " not done";
-			}
-			else {
-				cout << "done";
-			}
+        else
+        {
+            for (int i = 0; i < name.size(); i++)
+            {
+                if (isalpha(name[i]))
+                {
+                    count++;
+                }
+            }
+            if (count != name.length())
+            {
+                cout << " not done";
+            }
+            else
+            {
+                cout << "done";
+            }
 
-			this->name = name;
-		}
-	}
-	void setPassword(string password) {
-		if (password.length() < 5 || password.length() > 20) {
-			cout << "the password must be between 5 to 20 latter !" << endl;
-			return;
-		}
-		else {
-			this->password = password;
-			cout << "done";
-		}
+            this->name = name;
+        }
+    }
+    void setPassword(string password)
+    {
+        if (password.length() < 5 || password.length() > 20)
+        {
+            cout << "the password must be between 5 to 20 latter !" << endl;
+            return;
+        }
+        else
+        {
+            this->password = password;
+            cout << "done";
+        }
+    }
+    void setBalance(double balance)
+    {
+        if (balance < 1500)
+        {
+            cout << "the minimum of balance is 1500 !";
+            return;
+        }
+        else
+        {
+            this->balance = balance;
+        }
+    }
+    void setId(int id)
+    {
+        this->id = id;
+    }
 
-	}
-	void setBalance(double balance) {
-		if (balance < 1500) {
-			cout << "the minimum of balance is 1500 !";
-			return;
-		}
-		else {
-			this->balance = balance;
-		}
-	}
-	void setId(int id) {
-		this->id = id;
-	}
+    // getter:
+    string getName()
+    {
+        return this->name;
+    }
+    string getPassword()
+    {
+        return this->password;
+    }
+    double getBalance()
+    {
+        return this->balance;
+    }
+    int getId()
+    {
+        return this->id;
+    }
 
-	//getter:
-	string getName() {
-		return this->name;
-	}
-	string getPassword() {
-		return this->password;
-	}
-	double getBalance() {
-		return this->balance;
-	}
-	int getId() {
-		return this->id;
-	}
+    // Display function
+    void deposit(double amount)
+    {
+        balance += amount;
+        cout << " The deposit amount   : " << amount << endl;
+        cout << " The current balance  : " << balance << endl;
 
-	//Display function
-	void deposit(double amount) {
-		balance += amount;
-		cout << " The deposit amount   : " << amount << endl;
-		cout << " The current balance  : " << balance << endl;
+        cout << "----------------------------\n";
+    }
 
-		cout << "----------------------------\n";
-	}
+    void withdraw(double amount)
+    {
+        if (balance < amount)
+        {
+            cout << " Your balance is less than the amount you want to withdraw !" << endl;
+            return;
+        }
 
-	void withdraw(double amount) {
-		if (balance < amount) {
-			cout << " Your balance is less than the amount you want to withdraw !" << endl;
-			return;
-		}
+        balance -= amount;
+        cout << " The withdrawn amount : " << amount << endl;
+        cout << " The current balance  : " << balance << endl;
 
-		balance -= amount;
-		cout << " The withdrawn amount : " << amount << endl;
-		cout << " The current balance  : " << balance << endl;
+        cout << "----------------------------\n";
+    }
+    void transferTo(double amount, Client& recipient)
+    {
+        if (balance < amount)
+        {
+            cout << " Your balance is less than the amount you want to withdraw !" << endl;
+            return;
+        }
 
-		cout << "----------------------------\n";
-	}
-	void transferTo(double amount, Client& recipient) {
-		if (balance < amount) {
-			cout << " Your balance is less than the amount you want to withdraw !" << endl;
-			return;
-		}
+        balance -= amount;
+        recipient.balance += amount;
+        cout << " The amount transfered To : " << amount << endl;
+        cout << " The current balance      : " << balance << endl;
 
-		balance -= amount;
-		recipient.balance += amount;
-		cout << " The amount transfered To : " << amount << endl;
-		cout << " The current balance      : " << balance << endl;
-
-		cout << "----------------------------\n";
-	}
-	void checkBalance() {
-		cout << " The current balance : " << balance << endl;
-		cout << "----------------------------\n";
-	}
-	void Display() {
-		cout << "       welcome :-   " << endl << endl;
-		cout << " Name     : " << name << endl;
-		cout << " password : ************" << endl;
-		cout << " id       : " << id << endl;
-		cout << " Balance  : " << balance << endl;
-		cout << "----------------------------\n";
-	}
+        cout << "----------------------------\n";
+    }
+    void checkBalance()
+    {
+        cout << " The current balance : " << balance << endl;
+        cout << "----------------------------\n";
+    }
+    void Displayclientinfo()
+    {
+        cout << "       welcome :-   " << endl
+            << endl;
+        cout << " Name     : " << name << endl;
+        cout << " password : ************" << endl;
+        cout << " id       : " << id << endl;
+        cout << " Balance  : " << balance << endl;
+        cout << "----------------------------\n";
+    }
 };
 
-class Employee :public Person
+class Employee : public Person
 {
 private:
-	int id;
-	string name;
-	string password;
-	double salary;
+    double salary;
 
 public:
-	//constractor:
-	Employee() { this->salary = 0;}
-	Employee(int id, string name, string password, double salary):Person(name, password, id) {
-		this->salary = salary;
-	}
+    Employee() { this->salary = 0; }
+    Employee(string name, string password, int id, double salary) : Person(name, password, id), salary(salary) {}
 
-	// getter
-	double getSalary()
-	{
-		return salary;
-	}
-	int getId()
-	{
-		return id;
-	}
-	string getName()
-	{
-		return name;
-	}
-	string getPassword()
-	{
-		return password;
-	}
+    // getters
+    double getSalary()
+    {
+        return salary;
+    }
+    int getId()
+    {
+        return id;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getPassword()
+    {
+        return password;
+    }
 
-	// setter
-	void setSalary(double salary)
-	{
-		if (salary < 5000)
-		{
-			cout << "Salary must be greater than 5000 !" << endl;
-		}
-		else
-		{
-			this->salary = salary;
-		}
-	}
-	void setId(int id)
-	{
-		this->id = id;
-	}
-	void setName(string name)
-	{
-		if (name.length() < 5 || name.length() > 20)
-		{
-			cout << "Name must be between 8 and 20 characters !" << endl;
-		}
-		else
-		{
-			this->name = name;
-		}
-	}
-	void setPassword(string password)
-	{
-		if (password.length() < 8 || password.length() > 20)
-		{
-			cout << "Password must be between 8 and 20 characters !" << endl;
-		}
-		else
-		{
-			this->password = password;
-		}
-	}
+    // setters
+    void setSalary(double salary)
+    {
+        if (salary < 5000)
+        {
+            cout << "Salary must be greater than 5000" << endl;
+        }
+        else
+        {
+            this->salary = salary;
+        }
+    }
+    void setId(int id)
+    {
+        this->id = id;
+    }
+    void setName(string name)
+    {
+        if (name.length() < 5 || name.length() > 20)
+        {
+            cout << "Name must be between 8 and 20 characters" << endl;
+        }
+        else
+        {
+            this->name = name;
+        }
+    }
+    void setPassword(string password)
+    {
+        if (password.length() < 8 || password.length() > 20)
+        {
+            cout << "Password must be between 8 and 20 characters" << endl;
+        }
+        else
+        {
+            this->password = password;
+        }
+    }
 
-	// display
-	void display()
-	{
-		cout << "        Employee Information :- " << endl<<endl;
-		cout << " Name     : " << name << endl;
-		cout << " ID       : " << id << endl;
-		cout << " Password : " << password << endl;
-		cout << " Salary   : " << salary << endl;
-		cout << "----------------------------\n";
+    // display
+    void DisplayEmployeeinfo()
+    {
+        cout << "Name   : " << name << endl;
+        cout << "ID     : " << id << endl;
+        cout << "Salary : " << salary << endl;
+        cout << "------------------------------------" << endl;
+    }
+};
+class Admin : public Person {
+private:
 
-	}
+    double balance;
+
+public:
+    Admin() { this->balance = 0; }
+    Admin(string name, string password, int id, double balance) : Person(name, password, id), balance(balance) {}
+
+    // getters
+    double getBalance()
+    {
+        return balance;
+    }
+    int getId()
+    {
+        return id;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getPassword()
+    {
+        return password;
+    }
+
+    // setters
+    void setBalance(double balance)
+    {
+        if (balance < 5000)
+        {
+            cout << "balance must be greater than 5000" << endl;
+        }
+        else
+        {
+            this->balance = balance;
+        }
+    }
+    void setId(int id)
+    {
+        this->id = id;
+    }
+    void setName(string name)
+    {
+        if (name.length() < 5 || name.length() > 20)
+        {
+            cout << "Name must be between 8 and 20 characters" << endl;
+        }
+        else
+        {
+            this->name = name;
+        }
+    }
+    void setPassword(string password)
+    {
+        if (password.length() < 8 || password.length() > 20)
+        {
+            cout << "Password must be between 8 and 20 characters" << endl;
+        }
+        else
+        {
+            this->password = password;
+        }
+    }
+
+    // display
+    void DisplayAdmininfo()
+    {
+        cout << " Name    : " << name << endl;
+        cout << " ID      : " << id << endl;
+        cout << " Balance : " << balance << endl;
+        cout << "------------------------------------" << endl;
+    }
+
 };
 
 #endif
+
+
