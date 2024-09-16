512dcfab5fd3804882d6b3897023c8764a25aec8
youssefahmed10-top <yousssefahmed9@gmail.com>
2024-09-16 19:26:25 +0300

    Phase_1

--- Bank-System/Route-Project-main/Bank System.cpp
+++ Bank-System/Route-Project-main/Bank System.cpp
@@ -1,91 +1,103 @@
 
-    #include <iostream>
-    #include "Bank System.h"
-    using namespace std;
+#include <iostream>
+#include "Bank System.h"
+using namespace std;
+int main()
+{
+    //====================================================== For Client Class======================================================
+    Client c;
+    string name;
+    string password;
+    int balance;
+    int id;
+
+    cout << "Enter Client Name: ";
+    cin >> name;
+    c.setName(name);
+
+    cout << "Enter Client ID: ";
+    cin >> id;
+    c.setId(id);
+
+    cout << "Enter Client Password: ";
+    cin >> password;
+    c.setPassword(password);
+
+
+    cout << "Enter Client Balance: ";
+    cin >> balance;
+    c.setBalance(balance);
+    cout <<"+++++++++++++++++++++++++++++" <<endl;
 
 
 
-    int main()
-    {
-        //========================== For Client Class====================================
-        Client c;
-        string name;
-        string password;
-        int balance;
-        int id;
+    // c.Displayclientinfo();
 
-        cout << "Enter Client Name: ";
-        cin >> name;
-        c.setName(name);
 
-        cout << "Enter Client ID: ";
-        cin >> id;
-        c.setId(id);
+    //====================================================== For Employee Class======================================================
+    Employee e;
 
-        cout << "Enter Client Password: ";
-        cin >> password;
-        c.setPassword(password);
+    double salary;
 
-        cout << "Enter Client Balance: ";
-        cin >> balance;
-        c.setBalance(balance);
+    cout << "Enter Employee Name: ";
+    cin >> name;
+    e.setName(name);
 
-        c.Displayclientinfo();
+    cout << "Enter Employee ID: ";
+    cin >> id;
+    e.setId(id);
 
-        //============================== For Employee Class=================================
-        Employee e;
-
-        double salary;
-
-        cout << "Enter Employee Name: ";
-        cin >> name;
-        e.setName(name);
-
-        cout << "Enter Employee ID: ";
-        cin >> id;
-        e.setId(id);
-
-        cout << "Enter Employee Password: ";
-        cin >> password;
-        e.setPassword(password);
+    cout << "Enter Employee Password: ";
+    cin >> password;
+    e.setPassword(password);
 
 
 
-        cout << "Enter Employee Salary: ";
-        cin >> salary;
-        e.setSalary(salary);
-
-        e.DisplayEmployeeinfo();
-
-        //===============================Admin Class====================================
-
-        Admin a;
-
-        cout << "Enter Admin Name: ";
-        cin >> name;
-        a.setName(name);
+    cout << "Enter Employee Salary: ";
+    cin >> salary;
+    e.setSalary(salary);
+    cout <<"+++++++++++++++++++++++++++++" <<endl;
 
 
-        cout << "Enter Admin ID: ";
-        cin >> id;
-        a.setId(id);
 
+    // e.DisplayEmployeeinfo();
+    //==================================================================Admin Class===========================================================
 
-        cout << "Enter Admin Password: ";
-        cin >> password;
-        a.setPassword(password);
-
-        cout << "Enter Admin Balance: ";
-        cin >> balance;
-        a.setSalary(balance);
-
-        a.DisplayAdmininfo();
+    Admin a;
 
 
 
 
+    cout << "Enter Admin Name: ";
+    cin >> name;
+    a.setName(name);
+
+    cout << "Enter Admin ID: ";
+    cin >> id;
+    a.setId(id);
+
+    cout << "Enter Admin Password: ";
+    cin >> password;
+    a.setPassword(password);
 
 
 
-        return 0;
-    }
+    cout << "Enter Admin Balance: ";
+    cin >> balance;
+    a.setSalary(balance);
+
+
+    c.Displayclientinfo();
+    cout << endl;
+    e.DisplayEmployeeinfo();
+    cout << endl;
+    a.DisplayAdmininfo();
+
+
+
+
+
+
+
+    return 0;
+}
